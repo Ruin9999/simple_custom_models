7ea3e5989796a99a7c0ce659af5dea8185044622
Ruin9999 <b14ktoss@gmail.com>
2025-04-09 01:26:45 +0800

    Initial Commit

---
+++ src/main/java/dev/shunjieyong/simple_custom_models/Simple_custom_models.java
@@ -0,0 +1,10 @@
+package dev.shunjieyong.simple_custom_models;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Simple_custom_models implements ModInitializer {
+
+  @Override
+  public void onInitialize() {
+  }
+}
